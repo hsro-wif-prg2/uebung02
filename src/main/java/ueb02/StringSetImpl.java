39d1ce95dc4721c087d13b71d7230c2850482916
Anton <d1Av0lo3124@gmail.com>
2018-04-05 10:02:47 +0200

    test-loesung

--- src/main/java/ueb02/StringSetImpl.java
+++ src/main/java/ueb02/StringSetImpl.java
@@ -1,4 +1,36 @@
 package ueb02;
 
 public class StringSetImpl implements StringSet {
+ class Element {
+     String val;
+     Element left,right;
+
+     Element (String _val,Element _left,Element _right){
+         _val = val;
+         _left = left;
+         _right = _right;
+     }
+ }
+
+
+
+    @Override
+    public boolean add(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(String s) {
+        return false;
+    }
+
+    @Override
+    public String remove(String s) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
